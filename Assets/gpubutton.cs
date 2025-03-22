eb1b35526b74002edfafe05e444531ffcecffce9
kaspar1910 <kaspar1910@gmail.com>
2025-03-22 20:55:46 +0100

    hey

--- Assets/gpubutton.cs
+++ Assets/gpubutton.cs
@@ -13,4 +13,36 @@ public class gpubutton : MonoBehaviour
     {
         
     }
+    
+    public Transform placeholder;
+    public Transform placeholder2;
+
+    public static bool isAnyComponentOut = false;
+
+    public bool isOut = false;
+    
+    
+    
+    void OnMouseDown()
+    {
+        if (isOut == true)
+        {
+            transform.position = placeholder2.position;
+            isOut = false;
+            isAnyComponentOut = false;
+        }
+
+        else if (isOut == false && isAnyComponentOut == true)
+        {
+        }
+        
+        else if (isAnyComponentOut == false)
+        {
+            {
+                transform.position = placeholder.position;
+            }
+            isOut = true;
+            isAnyComponentOut = true;
+        }
+    }
 }
